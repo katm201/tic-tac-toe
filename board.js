5217f60037d1ea6eb8f26c46a57158b91933ec85
Katherine Manning <katm201@gmail.com>
2018-01-08 15:15:02 -0800

    (feat) add play methods to Board class

--- board.js
+++ board.js
@@ -2,7 +2,51 @@
 class Board {
   constructor() {
     this.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
+    this.players = { true: 'x', false: 'o' };
+    this.turn = true;
   }
+
+  play(row, col) {
+    if (!this.checkValidPlay(row, col)) { return 'invalid'; }
+    
+    const piece = this.players[this.turn];
+    this.board[row][col] = piece;
+    this.turn = !this.turn;
+    this.render();
+    
+    if (checkForWin()) { return 'win'; }
+
+    return 'next';
+  }
+
+  checkValidPlay(row, col) {
+    return this.board[row][col] === ' ';
+  }
+
+  checkRow(row) {
+    const piece = row[0]
+    for (let i = 1; i < 3; i++) {
+      if (row[i] !== piece) { return false; }
+    }
+    return true;
+  }
+
+  checkDiagonals(corner) {
+    const piece = this.board[0][corner];
+    const direction = corner === 0 ? 1 : -1;
+    if (this.board[1][1] !== piece) { return false; }
+    if (this.board[2][1 + direction] !== piece) { return false; }
+    return true; 
+  }
+
+  checkForWin() {
+    for (let i = 0; i < 3; i++) {
+      if (checkRow(i)) { return true; }
+    }
+    if (checkDiagonals(0) || checkDiagonals(2)) { return true; }
+    return false;
+  }
+
   render() {
     let view = ''
     for (let i = 0; i < 3; i++) {
@@ -13,7 +57,7 @@ class Board {
       }
       view += '\n';
     }
-  return view;
+  console.log(view);
   }
 }
 
--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const prompt = require('prompt');
 const Board = require('./board');
 
 const game = new Board();
-console.log(game.render())
+console.log(game.play(1,1));
+console.log(game.play(2,2));
